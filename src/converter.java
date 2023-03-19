0a54a3d33ee54529c81a876da38593b0732bf142
shaba <shaba@192.168.0.24>
2023-03-19 18:53:39 -0500

    testing from local machine to remote machine

---
+++ src/converter.java
@@ -0,0 +1,14 @@
+
+public class converter {
+
+	public static void main(String[] args)
+	{
+		int i = 0;
+		byte b1 = (byte)i;
+		
+		
+		System.out.println(i);
+		System.out.println(b1);
+	}
+
+}
---
+++ src/testbyte.java
@@ -0,0 +1,20 @@
+
+public class testbyte {
+
+	public static void main(String[] args)
+	
+	{
+		
+	 //  byte b1 = (byte)300;
+		int i = 128;
+		byte b1=(byte)i ;
+	//	int j = 20;
+		//int k = 0X20;
+		
+	
+	    System.out.println(i);
+		//System.out.println(j);
+	//	System.out.println(k);
+	}
+
+}
