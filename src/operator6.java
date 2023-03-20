cb0f322714a065014cf3e6bb7e1ca21ce9367dda
shaba <shaba@192.168.0.24>
2023-03-19 21:34:53 -0500

    operator 6
    output:
    
    13
    57

---
+++ src/operator6.java
@@ -0,0 +1,18 @@
+
+public class operator6 {
+
+	public static void main(String[] args)
+	
+	{
+		int i = 10;
+
+		int j = i++ + i++ + --i + ++i + ++i; 
+
+		System.out.println(i); 
+
+		System.out.println(j);
+
+
+	}
+
+}
