3b3049375716eda34ad59a7b611ae8281398224e
shaba <shaba@192.168.0.24>
2023-03-19 21:41:10 -0500

    unary operator:
    output:
    
    13
    57

---
+++ src/unary_operator.java
@@ -0,0 +1,43 @@
+
+public class unary_operator {
+
+	public static void main(String[] args)
+	
+	{
+		int i1 = -10;
+
+	      System.out.println(i1);
+
+	      
+
+	      int i2 = 10;
+
+	      System.out.println(i2++);
+
+
+
+	      int i3 = 10;
+
+	      System.out.println(++i3);
+
+
+
+	      int i4 = 10;
+
+	      System.out.println(i4--);
+
+
+
+	      int i5 = 10;
+
+	      System.out.println(--i5);
+
+	  
+
+	      int i6 = 10;
+
+	      System.out.println(~i6);
+
+	}
+
+}
