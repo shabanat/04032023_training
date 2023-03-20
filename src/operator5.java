b555ac3621bc49d6ba25cb29bf6d8ca688e28787
shaba <shaba@192.168.0.24>
2023-03-19 21:31:49 -0500

    operator 5
    output:
    
    12
    22

---
+++ src/operator5.java
@@ -0,0 +1,17 @@
+
+public class operator5 {
+
+	public static void main(String[] args)
+	
+	{
+		int i = 10; 
+
+		int j = i++ + ++i; 
+
+		System.out.println(i); 
+
+		System.out.println(j);
+
+	}
+
+}
