03acaa011b201b9d32663ada6b8ee9a40afd2a04
shaba <shaba@192.168.0.24>
2023-03-19 20:16:25 -0500

    operator assigment2

---
+++ src/operater2.java
@@ -0,0 +1,23 @@
+
+public class operater2 {
+
+	public static void main(String[] args)
+	
+	{
+		int i = 10;
+		int j= 7; 
+
+		double db1 = i/j; 
+
+		double db2 = (double)i/j; 
+
+		double db3 = i/(double)j;
+		
+		System.out.println(db1); 
+
+		System.out.println(db2); 
+
+		System.out.println(db3); 
+	}
+
+}
