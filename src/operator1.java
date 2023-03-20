d03e4f41cb1329be7f57449c6462a30df4d13203
shaba <shaba@192.168.0.24>
2023-03-19 20:03:28 -0500

    output:
    17
    3
    70
    1
    3

---
+++ src/operator1.java
@@ -0,0 +1,23 @@
+
+public class operator1 {
+
+	public static void main(String[] args)
+	{
+		int i = 10; 
+
+		int j = 7; 
+
+		System.out.println(i+j); 
+
+		System.out.println(i-j); 
+
+		System.out.println(i*j); 
+
+		System.out.println(i/j); 
+
+		System.out.println(i%j);
+
+
+	}
+
+}
